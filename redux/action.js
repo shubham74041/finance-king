b52ad0d545e209e09d57f8a1f4d15ef24e14d2fd
shubham74041 <shubham.in0181@gmail.com>
2024-04-22 23:15:20 +0530

    first commit

--- redux/action.js
+++
@@ -1,7 +0,0 @@
-// actions.js
-export const updateUserData = (userData) => {
-  return {
-    type: "UPDATE_USER_DATA",
-    payload: userData,
-  };
-};
--- store.js
+++
@@ -1,8 +0,0 @@
-// store.js
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./redux/reducer";
-
-const store = createStore(rootReducer, applyMiddleware(thunk.default));
-
-export default store;
