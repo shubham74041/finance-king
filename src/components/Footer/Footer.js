03d79da24d07cbfa5d0fc14eb336771da27e2639
shubham74041 <shubham.in0181@gmail.com>
2024-04-28 15:06:25 +0530

    feat/ fix ui

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,39 +2,48 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Footer.css"; // Import your CSS file for Footer styling
 
-import contactIcon from "../icons/contact.svg";
-import productIcon from "../icons/product.svg";
-import userIcon from "../icons/user.svg";
-import homeIcon from "../icons/home-icon.svg";
+import contactIcon from "../icons/contact.png";
+import productIcon from "../icons/product.png";
+import userIcon from "../icons/user.png";
+import homeIcon from "../icons/home-icon.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <ul className="footer-list">
+      <li className="footer-item">
+  <Link to="/" className="footer-link">
+    <div className="footer-icon-container">
+      <img src={homeIcon} alt="Home" className="footer-icon" />
+      <span className="icon-name">Home</span>
+    </div>
+  </Link>
+</li>
+
+<li className="footer-item">
+  <Link to="/contact" className="footer-link">
+    <div className="footer-icon-container">
+      <img src={contactIcon} alt="Contact" className="footer-icon" />
+      <span className="icon-name">Contact</span>
+    </div>
+  </Link>
+</li>
+<li className="footer-item">
+  <Link to="/product-details" className="footer-link">
+    <div className="footer-icon-container">
+      <img src={productIcon} alt="Product Details" className="footer-icon" />
+      <span className="icon-name">Product</span>
+    </div>
+  </Link>
+</li>
         <li className="footer-item">
-          <Link to="/" className="footer-link">
-            <img src={homeIcon} alt="Home" className="footer-icon " />
-          </Link>
-        </li>
-        <li className="footer-item">
-          <Link to="/contact" className="footer-link">
-            <img src={contactIcon} alt="Contact" className="footer-icon" />
-          </Link>
-        </li>
-        <li className="footer-item">
-          <Link to="/product-details" className="footer-link">
-            <img
-              src={productIcon}
-              alt="Product Details"
-              className="footer-icon"
-            />
-          </Link>
-        </li>
-        <li className="footer-item">
-          <Link to="/total-amount" className="footer-link">
-            <img src={userIcon} alt="User Profile" className="footer-icon" />
-          </Link>
-        </li>
+  <Link to="/total-amount" className="footer-link">
+    <div className="footer-icon-container">
+      <img src={userIcon} alt="User Profile" className="footer-icon" />
+      <span className="icon-name">Profile</span>
+    </div>
+  </Link>
+</li>
       </ul>
     </footer>
   );
