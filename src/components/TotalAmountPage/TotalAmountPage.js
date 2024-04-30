b01bdba15d50f9657a59b10ca678f4e339f0e39b
Piyush <kumarpiyush2909@gmail.com>
2024-04-30 12:49:33 +0530

    feat/ profile ui

--- src/components/TotalAmountPage/TotalAmountPage.js
+++ src/components/TotalAmountPage/TotalAmountPage.js
@@ -4,11 +4,15 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./TotalAmountPage.css";
 import HomeCard from "../HomeCard/HomeCard";
-import { FaAngleRight } from "react-icons/fa";
+// import { FaAngleRight } from "react-icons/fa";
 import withdrawalIcon from "../icons/withdrawalIcon.png";
+import recordIcon from "../icons/order-icon.png";
+import financialIcon from "../icons/icons8-financial-100.png";
+import followIcon from "../icons/icons8-telegram-100.png";
+import supportIcon from "../icons/contact.png";
+import complainIcon from "../icons/icons8-complaint-100.png";
+import signoutIcon from "../icons/icons8-logout-100.png";
 // import Support from "../icons/support.svg";
-
-
 
 const TotalAmountPage = () => {
   const navigate = useNavigate();
@@ -22,50 +26,89 @@ const TotalAmountPage = () => {
       <HomeCard />
       <div className="additional-links">
         <div className="link" onClick={() => handleNavigate("/order")}>
-          <img src={withdrawalIcon} alt="Withdrawal" className="" style={{ width: "30px", height: "30px" }} />
+          <img
+            src={recordIcon}
+            alt="Withdrawal"
+            className=""
+            style={{ width: "30px", height: "30px", marginRight: "10px" }}
+          />
           <div className="common-space">
-
-          <span>Order Record</span>
-          <FaAngleRight />
+            <span>Order Record</span>
+            {/* <FaAngleRight /> */}
           </div>
         </div>
 
         <div className="link" onClick={() => handleNavigate("/financial")}>
-        <img src={withdrawalIcon} alt="Withdrawal" className="" style={{ width: "30px", height: "30px" }} />
-        <div className="common-space">
-          <span>Financial Details</span>
-          <FaAngleRight />
+          <img
+            src={financialIcon}
+            alt="Withdrawal"
+            className=""
+            style={{ width: "30px", height: "30px", marginRight: "10px" }}
+          />
+          <div className="common-space">
+            <span>Financial Details</span>
+            {/* <FaAngleRight /> */}
+          </div>
         </div>
-        </div>
-
+        {/* 
         <div className="link" onClick={() => handleNavigate("/download")}>
-          <img src={withdrawalIcon} alt="Withdrawal" className="" style={{width:"30px",height:"30px"}} />
+          <img
+            src={withdrawalIcon}
+            alt="Withdrawal"
+            className=""
+            style={{ width: "30px", height: "30px", marginRight: "10px" }}
+          />
           <span>Downloads</span>
           <FaAngleRight />
-        </div>
+        </div> */}
 
         <div className="link" onClick={() => handleNavigate("/follow")}>
-          <img src={withdrawalIcon} alt="Withdrawal" className="" style={{width:"30px",height:"30px"}} />
+          <img
+            src={followIcon}
+            alt="Withdrawal"
+            className=""
+            style={{ width: "30px", height: "30px", marginRight: "10px" }}
+          />
           <span>Follow us</span>
-          <FaAngleRight />
+          {/* <FaAngleRight /> */}
         </div>
 
         <div className="link" onClick={() => handleNavigate("/support")}>
-          <img src={withdrawalIcon} alt="Withdrawal" className="" style={{width:"30px",height:"30px"}} />
+          <img
+            src={supportIcon}
+            alt="Withdrawal"
+            className=""
+            style={{
+              width: "30px",
+              height: "30px",
+              backgroundColor: "black",
+              marginRight: "10px",
+            }}
+          />
           <span>Support</span>
-          <FaAngleRight />
+          {/* <FaAngleRight /> */}
         </div>
 
         <div className="link" onClick={() => handleNavigate("/complaint")}>
-          <img src={withdrawalIcon} alt="Withdrawal" className="" style={{width:"30px",height:"30px"}} />
+          <img
+            src={complainIcon}
+            alt="Withdrawal"
+            className=""
+            style={{ width: "30px", height: "30px", marginRight: "10px" }}
+          />
           <span>Complaint</span>
-          <FaAngleRight />
+          {/* <FaAngleRight /> */}
         </div>
 
         <div className="link" onClick={() => handleNavigate("/logout")}>
-          <img src={withdrawalIcon} alt="Withdrawal" className="" style={{width:"30px",height:"30px"}} />
+          <img
+            src={signoutIcon}
+            alt="Withdrawal"
+            className=""
+            style={{ width: "30px", height: "30px", marginRight: "10px" }}
+          />
           <span>Sign Out</span>
-          <FaAngleRight />
+          {/* <FaAngleRight /> */}
         </div>
       </div>
     </div>
