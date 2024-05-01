f0a3c8cd0f35390c5043f434d83c9b79a913855c
Piyush <kumarpiyush2909@gmail.com>
2024-05-01 22:53:31 +0530

    feat/update img

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,6 +4,12 @@ import GenericCard from "../GenericCard/GenericCard";
 import "./HomePage.css"; // Import CSS file for styling
 // import NavBar from "../NavBar/NavBar";
 import HomeCard from "../HomeCard/HomeCard";
+import Img1 from "../icons/img1.jpg";
+import Img2 from "../icons/img2.jpg";
+import Img3 from "../icons/img3.jpg";
+import Img4 from "../icons/img4.jpg";
+import Img5 from "../icons/img5.jpg";
+import Img6 from "../icons/img6.jpg";
 
 const HomePage = ({ cards }) => {
   // Dummy card data
@@ -14,8 +20,8 @@ const HomePage = ({ cards }) => {
       dailyIncome: 100,
       totalAmount: 1000,
       cycle: "90 days",
-      image:
-        "https://images.unsplash.com/photo-1572635196237-14b3f281503f?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: Img1,
+      // "https://images.unsplash.com/photo-1572635196237-14b3f281503f?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       id: 2,
@@ -24,8 +30,8 @@ const HomePage = ({ cards }) => {
       dailyIncome: 100,
       totalAmount: 1000,
       cycle: "90 days",
-      image:
-        "https://images.unsplash.com/photo-1581235720704-06d3acfcb36f?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: Img2,
+      // "https://images.unsplash.com/photo-1581235720704-06d3acfcb36f?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       id: 3,
@@ -34,8 +40,8 @@ const HomePage = ({ cards }) => {
       dailyIncome: 100,
       totalAmount: 1000,
       cycle: "90 days",
-      image:
-        "https://images.unsplash.com/photo-1564466809058-bf4114d55352?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: Img3,
+      // "https://images.unsplash.com/photo-1564466809058-bf4114d55352?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       id: 4,
@@ -44,8 +50,8 @@ const HomePage = ({ cards }) => {
       dailyIncome: 100,
       totalAmount: 1000,
       cycle: "90 days",
-      image:
-        "https://images.unsplash.com/photo-1503602642458-232111445657?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: Img4,
+      // "https://images.unsplash.com/photo-1503602642458-232111445657?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       id: 5,
@@ -54,8 +60,8 @@ const HomePage = ({ cards }) => {
       dailyIncome: 100,
       totalAmount: 1000,
       cycle: "90 days",
-      image:
-        "https://plus.unsplash.com/premium_photo-1681139760816-d0c39952f9ac?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: Img5,
+      // "https://plus.unsplash.com/premium_photo-1681139760816-d0c39952f9ac?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       id: 6,
@@ -64,30 +70,30 @@ const HomePage = ({ cards }) => {
       dailyIncome: 100,
       totalAmount: 1000,
       cycle: "90 days",
-      image:
-        "https://images.unsplash.com/photo-1583394838336-acd977736f90?q=80&w=1884&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: Img6,
+      // "https://images.unsplash.com/photo-1583394838336-acd977736f90?q=80&w=1884&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
-    {
-      id: 7,
-      title: "Plan G",
-      // description: "This is a description for card 2.",
-      dailyIncome: 100,
-      totalAmount: 1000,
-      cycle: "90 days",
-      image:
-        "https://images.unsplash.com/photo-1524805444758-089113d48a6d?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 7,
-      title: "Plan H",
-      // description: "This is a description for card 2.",
-      dailyIncome: 100,
-      totalAmount: 1000,
-      cycle: "90 days",
-      image:
-        "https://images.unsplash.com/photo-1524805444758-089113d48a6d?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    // Add more dummy card objects as needed
+    // {
+    //   id: 7,
+    //   title: "Plan G",
+    //   description: "This is a description for card 2.",
+    //   dailyIncome: 100,
+    //   totalAmount: 1000,
+    //   cycle: "90 days",
+    //   image: Img3,
+    //   // "https://images.unsplash.com/photo-1524805444758-089113d48a6d?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // },
+    // {
+    //   id: 7,
+    //   title: "Plan H",
+    //   // description: "This is a description for card 2.",
+    //   dailyIncome: 100,
+    //   totalAmount: 1000,
+    //   cycle: "90 days",
+    //   image: Img4,
+    //   // "https://images.unsplash.com/photo-1524805444758-089113d48a6d?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // },
+    // // Add more dummy card objects as needed
   ];
 
   return (
@@ -109,9 +115,12 @@ const HomePage = ({ cards }) => {
               </div>
 
               <div className="img">
-  <img src={card.image} alt={card.title} className="responsive-image" />
-</div>
-
+                <img
+                  src={card.image}
+                  alt={card.title}
+                  className="responsive-image"
+                />
+              </div>
             </div>
             <div className="card-details">
               <div className="card-details1">
