71c98590320f2a72afecee5dbf8fe11c35b89ade
Piyush <kumarpiyush2909@gmail.com>
2024-05-31 19:16:25 +0530

    feat/ update admin

--- src/components/AdminPage/Admin/ContactInfo.js
+++ src/components/AdminPage/Admin/ContactInfo.js
@@ -3,15 +3,34 @@ import axios from "axios";
 import "./ContactInfoPage.css"; // Import your CSS file for styling
 
 const ContactInfoPage = () => {
-  const [contactData, setContactData] = useState([]);
+  const [contactData, setContactData] = useState(null); // Initialize to null
+  const [error, setError] = useState(null); // State to handle errors
 
   useEffect(() => {
     const id = localStorage.getItem("site");
     axios
-      .get(`https://rajjiowin-backend.vercel.app/${id}`)
-      .then((response) => setContactData(response.data))
-      .catch((error) => console.error("Error fetching data:", error));
+      .get(`https://rajjiowin-backend.vercel.app/messages/${id}`)
+      .then((response) => {
+        if (Array.isArray(response.data)) {
+          setContactData(response.data);
+        } else {
+          setContactData([]);
+          console.error("Data fetched is not an array:", response.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setError("Failed to fetch data");
+      });
   }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (contactData === null) {
+    return <div>Loading...</div>; // Add a loading state
+  }
 
   return (
     <div className="contact-info-container">
@@ -29,7 +48,7 @@ const ContactInfoPage = () => {
         </thead>
         <tbody>
           {contactData.map((contact) => (
-            <tr key={contact.userId}>
+            <tr key={contact._id}>
               <td data-label="UserID">{contact.userId}</td>
               <td data-label="Name">{contact.name}</td>
               <td data-label="Email">{contact.email}</td>
