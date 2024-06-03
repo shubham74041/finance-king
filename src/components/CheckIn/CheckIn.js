2fd52dc682c916569abd83db9d8c07e577e6d202
Piyush <kumarpiyush2909@gmail.com>
2024-06-03 14:23:00 +0530

    feat/update

--- src/components/CheckIn/CheckIn.js
+++ src/components/CheckIn/CheckIn.js
@@ -9,32 +9,12 @@ const CheckIn = ({ enabled, setEnabled }) => {
   const [lastCheckInDate, setLastCheckInDate] = useState("");
 
   useEffect(() => {
-    // Load last check-in date from local storage
     const lastCheckIn = localStorage.getItem("lastCheckIn");
     setLastCheckInDate(lastCheckIn);
 
-    // Set button color based on whether the user has already checked in today
     const today = new Date().toISOString().split("T")[0];
     setButtonColor(lastCheckIn === today ? "checked-in" : "default");
-  }, []);
-
-  useEffect(() => {
-    // Check if it's a new day to enable the check-in button
-    const checkNewDay = () => {
-      const now = new Date();
-      const nextCheckInTime = new Date();
-      nextCheckInTime.setHours(0, 1, 0, 0); // Set next check-in time to 12:01 am
-
-      if (now >= nextCheckInTime) {
-        setButtonColor("default"); // Reset button color for new day
-        setEnabled(true); // Enable check-in button
-      }
-    };
-
-    const intervalId = setInterval(checkNewDay, 60000); // Check every minute
-
-    return () => clearInterval(intervalId); // Cleanup on component unmount
-  }, [setEnabled]);
+  }, [lastCheckInDate]);
 
   const handleCheckIn = async () => {
     if (buttonColor === "checked-in") {
@@ -49,7 +29,6 @@ const CheckIn = ({ enabled, setEnabled }) => {
     const today = new Date().toISOString().split("T")[0];
 
     try {
-      console.log("Request payload:", { userId }); // Log the request payload
       const response = await axios.post(
         `https://rajjiowin-backend.vercel.app/check-in/${userId}`,
         {}
@@ -60,13 +39,12 @@ const CheckIn = ({ enabled, setEnabled }) => {
       if (data.hasProducts) {
         setMessage("Today's check-in is complete");
         localStorage.setItem("lastCheckIn", today);
-        setEnabled(false); // Disable check-in button
-        setButtonColor("checked-in"); // Change button color
+        setEnabled(false);
+        setButtonColor("checked-in");
       } else {
         setMessage("You don't have any products");
       }
 
-      // Make the popup disappear after 10 seconds
       setTimeout(() => {
         setMessage("");
       }, 10000);
@@ -74,7 +52,6 @@ const CheckIn = ({ enabled, setEnabled }) => {
       console.error("Error during check-in:", error);
       setMessage("An error occurred during check-in. Please try again later.");
 
-      // Make the popup disappear after 10 seconds
       setTimeout(() => {
         setMessage("");
       }, 10000);
