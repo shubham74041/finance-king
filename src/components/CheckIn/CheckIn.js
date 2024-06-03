b03faaa980e9b3edf76534c866f7ea5cced168c8
Piyush <kumarpiyush2909@gmail.com>
2024-06-03 13:47:26 +0530

    feat/ update check-in

--- src/components/CheckIn/CheckIn.js
+++ src/components/CheckIn/CheckIn.js
@@ -8,17 +8,15 @@ const CheckIn = ({ enabled, setEnabled }) => {
   const [buttonColor, setButtonColor] = useState("default");
 
   useEffect(() => {
-    console.log("CheckIn component mounted, enabled:", enabled);
     const lastCheckIn = localStorage.getItem("lastCheckIn");
     const today = new Date().toISOString().split("T")[0]; // Get current date in YYYY-MM-DD format
 
     if (lastCheckIn === today) {
       setButtonColor("checked-in");
-      setEnabled(false);
     } else {
       setButtonColor(enabled ? "default" : "disabled");
     }
-  }, [enabled, setEnabled]);
+  }, [enabled]);
 
   useEffect(() => {
     const checkNewDay = () => {
@@ -44,7 +42,6 @@ const CheckIn = ({ enabled, setEnabled }) => {
       setMessage("You have already checked in today.");
       setTimeout(() => {
         setMessage("");
-        window.location.reload(); // Reload the window after the message disappears
       }, 10000);
       return;
     }
@@ -61,24 +58,25 @@ const CheckIn = ({ enabled, setEnabled }) => {
 
       if (data.hasProducts) {
         setButtonColor("checked-in");
-        setMessage("Today check-in complete");
+        setMessage("Today's check-in is complete");
         localStorage.setItem("lastCheckIn", today);
         setEnabled(false); // Disable check-in button
       } else {
         setButtonColor("default");
         setMessage("You don't have any products");
       }
+
       // Make the popup disappear after 10 seconds
       setTimeout(() => {
         setMessage("");
-        window.location.reload(); // Reload the window after the message disappears
       }, 10000);
     } catch (error) {
       console.error("Error during check-in:", error);
       setMessage("An error occurred during check-in. Please try again later.");
+
+      // Make the popup disappear after 10 seconds
       setTimeout(() => {
         setMessage("");
-        window.location.reload(); // Reload the window after the message disappears
       }, 10000);
     }
   };
@@ -89,7 +87,7 @@ const CheckIn = ({ enabled, setEnabled }) => {
         <button
           onClick={handleCheckIn}
           className={`check_in_button ${buttonColor}`}
-          disabled={!enabled || buttonColor === "checked-in"}
+          disabled={buttonColor === "checked-in"}
         >
           <span className="title_check">Check-in</span>
         </button>
