a62115ee969b41171eeebfb1fbc26f8d48492d4c
Piyush <kumarpiyush2909@gmail.com>
2024-07-09 22:41:00 +0530

    feat/ add admin pagination

--- src/components/AdminPage/Admin/UserDetails.js
+++ src/components/AdminPage/Admin/UserDetails.js
@@ -1,26 +1,234 @@
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import "./UserDetails.css"; // Import the CSS file
+// import Notification from "../../Notification/Notification.js";
+
+// const UserDetails = () => {
+//   const [users, setUsers] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const [notification, setNotification] = useState(null);
+//   const [passwords, setPasswords] = useState({}); // State to manage password inputs
+
+//   useEffect(() => {
+//     const id = localStorage.getItem("site") || 9876543210;
+//     if (id) {
+//       axios
+//         .get(`https://rajjiowin-backend.vercel.app/users/${id}`)
+//         .then((response) => {
+//           console.log("API response: Done");
+//           if (Array.isArray(response.data)) {
+//             setUsers(response.data);
+//             // Initialize passwords state with current passwords
+//             const initialPasswords = response.data.reduce((acc, user) => {
+//               acc[user.userId] = user.userPassword;
+//               return acc;
+//             }, {});
+//             setPasswords(initialPasswords);
+//           } else {
+//             console.error("Unexpected response data format:", response.data);
+//             setUsers([]);
+//           }
+//         })
+//         .catch((error) => {
+//           console.error("There was an error fetching the user data!", error);
+//           setUsers([]);
+//         })
+//         .finally(() => {
+//           setLoading(false);
+//         });
+//     } else {
+//       console.error("No site ID found in local storage");
+//       setLoading(false);
+//       setNotification("No site ID found in local storage");
+//     }
+//   }, []);
+
+//   const handleAddAmount = (userId, amount) => {
+//     axios
+//       .post(`https://rajjiowin-backend.vercel.app/users/${userId}`, { amount })
+//       .then((response) => {
+//         console.log("API response: for handleAddAmount");
+//         setNotification(response.data.resMsg);
+//       })
+//       .catch((error) => {
+//         console.error("Error adding amount:", error);
+//       });
+//   };
+
+//   const handlePasswordChange = (userId, newPassword) => {
+//     setPasswords((prevPasswords) => ({
+//       ...prevPasswords,
+//       [userId]: newPassword,
+//     }));
+//   };
+
+//   const handlePasswordSave = (userId) => {
+//     const newPassword = passwords[userId];
+//     axios
+//       .post(`https://rajjiowin-backend.vercel.app/password/${userId}`, {
+//         newPassword,
+//       })
+//       .then((response) => {
+//         // console.log("Password update response:", response.data);
+//         setNotification(response.data.resMsg);
+//       })
+//       .catch((error) => {
+//         console.error("Error updating password:", error);
+//       });
+//   };
+
+//   const handleSearch = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   const filteredUsers = users.filter(
+//     (user) =>
+//       user.userId.toLowerCase().includes(searchTerm.toLowerCase()) ||
+//       (user.referralId &&
+//         user.referralId.toLowerCase().includes(searchTerm.toLowerCase()))
+//   );
+
+//   return (
+//     <div className="container">
+//       <h1 className="title">User Details</h1>
+//       <input
+//         type="text"
+//         placeholder="Search by User ID or Referral ID"
+//         value={searchTerm}
+//         onChange={handleSearch}
+//         className="search-input"
+//       />
+//       {loading ? (
+//         <p>Loading...</p>
+//       ) : (
+//         <>
+//           {notification && (
+//             <Notification
+//               message={notification}
+//               onClose={() => setNotification(null)}
+//             />
+//           )}
+//           <table className="user-table">
+//             <thead>
+//               <tr>
+//                 <th className="table-header">Serial No</th>
+//                 <th className="table-header">User ID</th>
+//                 <th className="table-header">Password</th>
+//                 <th className="table-header">Referral ID</th>
+//                 <th className="table-header">Add Amount</th>
+//                 <th className="table-header">Referral Count</th>
+//                 <th className="table-header">Referral Used</th>
+//                 <th className="table-header">Order Detail</th>
+//                 <th className="table-header">Referral Amount</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {filteredUsers.length > 0 ? (
+//                 filteredUsers.map((user, index) => (
+//                   <tr key={user.userId}>
+//                     <td>{index + 1}</td>
+//                     <td>{user.userId}</td>
+//                     <td>
+//                       <input
+//                         type="text"
+//                         value={passwords[user.userId] || ""}
+//                         onChange={(e) =>
+//                           handlePasswordChange(user.userId, e.target.value)
+//                         }
+//                         className="password-input"
+//                       />
+//                       <button
+//                         className="edit-password-button"
+//                         onClick={() => handlePasswordSave(user.userId)}
+//                       >
+//                         Edit
+//                       </button>
+//                     </td>
+//                     <td>{user.referralId || "No referral ID"}</td>
+//                     <td>
+//                       <input
+//                         className="amount-input"
+//                         id={`amount-${user.userId}`}
+//                         type="number"
+//                         placeholder="Enter amount"
+//                       />
+//                       <button
+//                         className="add-amount-button"
+//                         onClick={() => {
+//                           const amount = parseInt(
+//                             document.getElementById(`amount-${user.userId}`)
+//                               .value
+//                           );
+//                           console.log(amount);
+//                           if (!isNaN(amount)) {
+//                             handleAddAmount(user.userId, amount);
+//                           }
+//                         }}
+//                       >
+//                         Add Amount
+//                       </button>
+//                     </td>
+//                     <td>{user.referralCount || 0}</td>
+//                     <td>{user.usedReferralCode || "None"} </td>
+//                     <td>
+//                       User Buy <b>{user.orderCount}</b> Plans
+//                     </td>
+//                     <td>{user.referralValue || 0}</td>
+//                   </tr>
+//                 ))
+//               ) : (
+//                 <tr>
+//                   <td colSpan="9">No users found</td>
+//                 </tr>
+//               )}
+//             </tbody>
+//           </table>
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default UserDetails;
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./UserDetails.css"; // Import the CSS file
 import Notification from "../../Notification/Notification.js";
+import { Pagination } from "antd";
 
 const UserDetails = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
   const [notification, setNotification] = useState(null);
-  const [passwords, setPasswords] = useState({}); // State to manage password inputs
+  const [passwords, setPasswords] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [totalItems, setTotalItems] = useState(0);
 
   useEffect(() => {
+    fetchUsers(currentPage, pageSize, searchTerm);
+  }, [currentPage, pageSize, searchTerm]);
+
+  const fetchUsers = (page, size, search) => {
+    setLoading(true);
     const id = localStorage.getItem("site") || 9876543210;
     if (id) {
       axios
-        .get(`https://rajjiowin-backend.vercel.app/users/${id}`)
+        .get(`https://rajjiowin-backend.vercel.app/users/${id}`, {
+          params: {
+            page,
+            size,
+            search,
+          },
+        })
         .then((response) => {
-          console.log("API response: Done");
-          if (Array.isArray(response.data)) {
-            setUsers(response.data);
-            // Initialize passwords state with current passwords
-            const initialPasswords = response.data.reduce((acc, user) => {
+          if (Array.isArray(response.data.users)) {
+            setUsers(response.data.users);
+            setTotalItems(response.data.totalItems);
+            const initialPasswords = response.data.users.reduce((acc, user) => {
               acc[user.userId] = user.userPassword;
               return acc;
             }, {});
@@ -42,13 +250,12 @@ const UserDetails = () => {
       setLoading(false);
       setNotification("No site ID found in local storage");
     }
-  }, []);
+  };
 
   const handleAddAmount = (userId, amount) => {
     axios
       .post(`https://rajjiowin-backend.vercel.app/users/${userId}`, { amount })
       .then((response) => {
-        console.log("API response: for handleAddAmount");
         setNotification(response.data.resMsg);
       })
       .catch((error) => {
@@ -70,7 +277,6 @@ const UserDetails = () => {
         newPassword,
       })
       .then((response) => {
-        // console.log("Password update response:", response.data);
         setNotification(response.data.resMsg);
       })
       .catch((error) => {
@@ -80,14 +286,13 @@ const UserDetails = () => {
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
+    setCurrentPage(1); // Reset to the first page on new search
   };
 
-  const filteredUsers = users.filter(
-    (user) =>
-      user.userId.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      (user.referralId &&
-        user.referralId.toLowerCase().includes(searchTerm.toLowerCase()))
-  );
+  const handlePageChange = (page, size) => {
+    setCurrentPage(page);
+    setPageSize(size);
+  };
 
   return (
     <div className="container">
@@ -124,10 +329,10 @@ const UserDetails = () => {
               </tr>
             </thead>
             <tbody>
-              {filteredUsers.length > 0 ? (
-                filteredUsers.map((user, index) => (
+              {users.length > 0 ? (
+                users.map((user, index) => (
                   <tr key={user.userId}>
-                    <td>{index + 1}</td>
+                    <td>{(currentPage - 1) * pageSize + index + 1}</td>
                     <td>{user.userId}</td>
                     <td>
                       <input
@@ -160,7 +365,6 @@ const UserDetails = () => {
                             document.getElementById(`amount-${user.userId}`)
                               .value
                           );
-                          console.log(amount);
                           if (!isNaN(amount)) {
                             handleAddAmount(user.userId, amount);
                           }
@@ -184,6 +388,15 @@ const UserDetails = () => {
               )}
             </tbody>
           </table>
+          <div className="pagination-container">
+            <Pagination
+              current={currentPage}
+              pageSize={pageSize}
+              total={totalItems}
+              onChange={handlePageChange}
+              showSizeChanger
+            />
+          </div>
         </>
       )}
     </div>
