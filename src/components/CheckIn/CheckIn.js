6ad4697167c3469affbe780efd94d1473ef63273
Piyush <kumarpiyush2909@gmail.com>
2024-07-22 09:06:09 +0530

    feat/ update

--- src/components/CheckIn/CheckIn.js
+++ src/components/CheckIn/CheckIn.js
@@ -1,99 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import "./CheckIn.css";
-// import CheckInIcon from "../icons/icons8-entrance-100.png";
-
-// const CheckIn = ({ setWalletBalance }) => {
-//   const [message, setMessage] = useState("");
-//   const [buttonEnabled, setButtonEnabled] = useState(false);
-
-//   const fetchCheckInStatus = async () => {
-//     const userId = localStorage.getItem("site");
-//     try {
-//       const response = await axios.get(
-//         `https://rajjiowin-backend.vercel.app/${userId}/check-in-status`
-//       );
-//       if (response.data.checkInStatus) {
-//         setButtonEnabled(true);
-//       } else {
-//         setButtonEnabled(false);
-//       }
-//     } catch (error) {
-//       console.error("Error fetching check-in status:", error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchCheckInStatus();
-
-//     // Polling mechanism to fetch check-in status every 10 seconds
-//     const intervalId = setInterval(fetchCheckInStatus, 3000); // Adjust the interval as needed
-
-//     // Cleanup interval on component unmount
-//     return () => clearInterval(intervalId);
-//   }, []);
-
-//   const handleCheckIn = async () => {
-//     const userId = localStorage.getItem("site");
-//     try {
-//       const response = await axios.post(
-//         `https://rajjiowin-backend.vercel.app/check-in/${userId}`,
-//         {}
-//       );
-//       const data = response.data;
-
-//       if (data.message.includes("check-in complete")) {
-//         setMessage("Checked in successfully!");
-//         setWalletBalance(data.walletBalance);
-//         setButtonEnabled(false); // Disable button after successful check-in
-//         // Notify other tabs or devices about the update
-//         localStorage.setItem("checkInUpdated", Date.now());
-//         window.location.reload();
-//       } else {
-//         setMessage(data.message);
-//       }
-//     } catch (error) {
-//       setMessage("Error during check-in. Please try again.");
-//     }
-//   };
-
-//   useEffect(() => {
-//     const handleStorageChange = (event) => {
-//       if (event.key === "checkInUpdated") {
-//         fetchCheckInStatus();
-//       }
-//     };
-
-//     window.addEventListener("storage", handleStorageChange);
-
-//     // Cleanup listener on component unmount
-//     return () => window.removeEventListener("storage", handleStorageChange);
-//   }, []);
-
-//   return (
-//     <div className="checkIn-container">
-//       <div className="check_title">
-//         <button
-//           onClick={handleCheckIn}
-//           className={`check_in_button ${
-//             buttonEnabled ? "default" : "disabled"
-//           }`}
-//           disabled={!buttonEnabled}
-//         >
-//           <span className="title_check">Check-in</span>
-//         </button>
-//         <span className="title_check2">Get rewards</span>
-//       </div>
-//       <div className="img_check">
-//         <img src={CheckInIcon} alt="checkIn-img" />
-//       </div>
-//       {message && <div className="popup">{message}</div>}
-//     </div>
-//   );
-// };
-
-// export default CheckIn;
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./CheckIn.css";
@@ -138,6 +42,8 @@ const CheckIn = ({ setWalletBalance }) => {
 
   const handleCheckIn = async () => {
     const userId = localStorage.getItem("site");
+    // Disable the button immediately to prevent double-clicks
+    setButtonEnabled(false);
     try {
       const response = await axios.post(
         `${process.env.REACT_APP_PATH_URL}/check-in/${userId}`,
@@ -149,7 +55,7 @@ const CheckIn = ({ setWalletBalance }) => {
         console.log("Check-in complete:", data);
         setMessage("Checked in successfully!");
         setWalletBalance(data.walletBalance);
-        setButtonEnabled(false); // Disable button after successful check-in
+        // setButtonEnabled(false); // Disable button after successful check-in
         const now = new Date();
         console.log("Updating lastCheckInDate after check-in:", now);
         setLastCheckInDate(now); // Update the last check-in date
@@ -184,8 +90,10 @@ const CheckIn = ({ setWalletBalance }) => {
     const now = new Date();
     console.log("Checking if it's a new day:", now, lastCheckInDate);
     if (
+      // lastCheckInDate &&
+      // now.toDateString() !== lastCheckInDate.toDateString()
       lastCheckInDate &&
-      now.toDateString() !== lastCheckInDate.toDateString()
+      now.toLocaleDateString() !== lastCheckInDate.toLocaleDateString()
     ) {
       console.log("New day detected, enabling button");
       setButtonEnabled(true);
