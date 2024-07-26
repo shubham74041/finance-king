b6dfb272025228906a9240ed628820e5c8c3d0e6
Piyush <kumarpiyush2909@gmail.com>
2024-07-26 21:05:57 +0530

    update refer url

--- src/components/ReferralPage/ReferralPage.js
+++ src/components/ReferralPage/ReferralPage.js
@@ -54,13 +54,13 @@ const ReferralPage = () => {
   };
 
   const handleSendWhatsApp = () => {
-    const referralLink = `https://rajjowin.in/signup?referralCode=${referralCode}`; // Updated with the correct query parameter name
+    const referralLink = `https://rajjiowin-backend.vercel.app/signup?referralCode=${referralCode}`; // Updated with the correct query parameter name
     const message = `Join me on this awesome platform using my referral code ${referralCode}! ${referralLink}`;
     window.open(`https://wa.me/?text=${encodeURIComponent(message)}`);
   };
 
   const handleSendTelegram = () => {
-    const referralLink = `https://rajjowin.in/signup?referralCode=${referralCode}`; // Updated with the correct query parameter name
+    const referralLink = `https://rajjiowin-backend.vercel.app/signup?referralCode=${referralCode}`; // Updated with the correct query parameter name
     const message = `Join me on this awesome platform using my referral code ${referralCode}! ${referralLink}`;
     window.open(
       `https://telegram.me/share/url?url=${encodeURIComponent(
