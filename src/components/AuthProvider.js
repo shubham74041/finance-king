5fb1bd5fc6326ca96d86a5069400b99fe1be39d1
shubham74041 <shubham.in0181@gmail.com>
2024-07-30 20:48:27 +0530

    login url

--- src/components/AuthProvider.js
+++ src/components/AuthProvider.js
@@ -16,7 +16,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (data) => {
     try {
-      const response = await fetch(`https://rajjowin.in/`, {
+      const response = await fetch(`https://rajjowin.in/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
