527bf73de1fdf5e605a8065bd4aad33be53a13b7
shubham74041 <shubham.in0181@gmail.com>
2024-08-01 23:20:46 +0530

    chnge

--- src/components/FinancialDetail/FinancialPage.js
+++ src/components/FinancialDetail/FinancialPage.js
@@ -11,7 +11,7 @@ const FinancialPage = () => {
   useEffect(() => {
     if (id) {
       axios
-        .get(`${process.env.REACT_APP_PATH_URL}/financial/${id}`)
+        .get(`https://rajjowin.in/financial/${id}`)
         .then((response) => {
           console.log("API response:", response.data);
           setFinancialData(response.data);
